26a84e195a1239cc054aa9a7f3d433eb1a3458e5
Madu-J Omenka <madu.omenka@hotmail.com>
2024-04-22 17:13:59 +0000

    Fixed Navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,99 @@
-import React from "react"
-import styles from "../styles/NavBar.module.css";
-import Navbar from "react-bootstrap/Navbar";
-import {Container, Navbar, Nav} from "react-bootstrap";
-import Nav from "react-bootstrap/Nav";
+import React from "react";
+import {Navbar, Container, Nav} from "react-bootstrap";
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
-    return (
-        <Navbar expand="md" fixed="top">
-            <Container>
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" height="45" />
-                    <div className={styles.Slogan}>
-                        <span>Welcome to Auctionghetto !</span>
-                    </div>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto text-left">
-                        <Nav.Link>
-                        <i className="fas fa-home"></i>Home
-                        </Nav.Link>
-                        <Nav.Link>
-                        <i className="fas fa-sign-in-alt"></i>Sign in
-                        </Nav.Link>
-                        <Nav.Link>
-                        <i className="fas fa-user-plus"></i>Sign up
-                        </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+    
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+    const auctionSaleIcon = (
+        <NavLink
+          className={styles.NavLink}
+          activeClassName={styles.Active}
+          to="/auctions/create"
+        >
+          <i className="far fa-plus-square"></i> Auction Sales
+        </NavLink>
+      );
+      const loggedInIcons = (
+        <>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/feed"
+          >
+            <i className="fas fa-stream"></i>Feed
+          </NavLink>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/bookmark"
+          >
+            <i className="fas fa-heart"></i>Bookmark
+          </NavLink>
+          <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+            <i className="fas fa-sign-out-alt"></i>Sign out
+          </NavLink>
+          <NavLink
+            className={styles.NavLink}
+            to={`/auctioneers/${currentUser?.auctioneer_id}`}
+          >
+            <Avatar src={currentUser?.auctioneer_image} text="Auctioneer" height={40} />
+          </NavLink>
+        </>
+      );
+      const loggedOutIcons = (
+        <>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/signin"
+          >
+            <i className="fas fa-sign-in-alt"></i>Sign in
+          </NavLink>
+          <NavLink
+            to="/signup"
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+          >
+            <i className="fas fa-user-plus"></i>Sign up
+          </NavLink>
+        </>
+      );
+      
+      return (
+      <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+      >
+    <Container>
+        <NavLink to="/">
+          <Navbar.Brand className="mr-0">
+            <img src={logo} alt="logo" height="45" />
+            <div className={styles.Slogan}>
+            <span>Welcome to Auctionghetto</span>
+            </div>
+          </Navbar.Brand>
+        </NavLink>
+        {currentUser && auctionSaleIcon }
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
