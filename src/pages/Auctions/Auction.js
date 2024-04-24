4eddecea0878e3b263441631ddbed05966bb9c8c
Madu-J Omenka <madu.omenka@hotmail.com>
2024-04-24 14:27:22 +0000

    Added auction.js and auctionCreateForm files

---
+++ src/pages/Auctions/Auction.js
@@ -0,0 +1,231 @@
+import React from "react";
+import { Row, Col, Media, Tooltip, Card, OverlayTrigger } from 'react-bootstrap';
+import { Link, useHistory  } from 'react-router-dom';
+import { axiosRes } from "../../api/axiosDefaults";
+import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import styles from "../../styles/Auction.module.css";
+
+// Component used for creating the Auctionghetto post information.
+function Auction(props) {
+  const {
+    id,
+    owner,
+    auctioneer_id,
+    auctioneer_image,
+    bookmark_id,
+    title,
+    productcategory,
+    item_products,
+    autocategory,
+    auctionstartday,
+    description,
+    year,
+    fueltype,
+    price,
+    image,
+    updated_at,
+    auctionPage,
+    setAuctions,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleBookmark = async () => {
+    try {
+      const { data } = await axiosRes.post("/bookmark/", { auction: id });
+      setAuctions((setAuctions) => ({
+        ...setAuctions,
+        results: setAuctions.results.map((auction) => {
+          return auction.id === id ? { ...auctionr, bookmark_id: data.id } : auction;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+   // Delete auction posting from bookmarked auctions
+  const handleUnbookmark = async () => {
+    try {
+      await axiosRes.delete(`/bookmarked/${bookmark_id}/`);
+      setAuctions((setAuctions) => ({
+        ...setAuctions,
+        results: setAuctions.results.map((auction) => {
+          return auction.id === id ? { ...auction, bookmark_id: null } : auction;
+        }),
+      }));
+    } catch (err) {
+      //console.log(err);
+    }
+  };
+
+  const handleEdit = () => {
+    history.push(`/auctions/${id}/edit`);
+  };
+
+  // Delete auctioghetto posting
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/auctions/${id}/`);
+      history.goBack();
+    } catch (err) {
+      //console.log(err);
+    }
+  };
+
+  return (
+    <Card className={styles.Auction}>
+      <Card.Body>
+        <Link to={`/auctions/${id}`}>
+          <Card.Img src={image} alt={title} />
+        </Link>
+      </Card.Body>
+
+      <div className={styles.PostBar}>
+        {is_owner ? (
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip>User cannot bookmark own listed item!</Tooltip>}
+          >
+            <i className="far fa-heart" />
+          </OverlayTrigger>
+        ) : bookmark_id ? (
+          <span onClick={handleUnbookmark}>
+            <i className={`fas fa-heart ${styles.Heart}`} />
+          </span>
+        ) : currentUser ? (
+          <span onClick={handleBookmark}>
+            <i className={`far fa-heart ${styles.HeartOutline}`} />
+          </span>
+        ) : (
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip>Log in to bookmark an auction!</Tooltip>}
+          >
+            <i className="far fa-heart" />
+          </OverlayTrigger>
+        )}
+      </div>
+
+      <Card.Body>
+        <Row className="mb-2 text-left no-gutters">
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-bookmark"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">Productcategory</span>
+                <span className="font-weight-bold text-capitalize">
+                  {brand}
+                </span>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-road"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">Item Products</span>
+                <span className="font-weight-bold">{item_products}</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-road"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">autocategory</span>
+                <span className="font-weight-bold">{autocategory}</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-road"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">auctionstartday</span>
+                <span className="font-weight-bold">{auctionstartday}</span>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-calendar-days"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">Year</span>
+                <span className="font-weight-bold">{year}</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="mb-5 text-left no-gutters">
+          <Col>
+
+          </Col>
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-gas-pump"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">Fueltype</span>
+                <span className="font-weight-bold text-capitalize">
+                  {fueltype}
+                </span>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col className="d-inline-block" xs={3}>
+                <i className="fa-solid fa-money-bill-1-wave"></i>
+              </Col>
+              <Col className="d-inline-block" xs={9}>
+                <span className="d-block text-muted">Price</span>
+                <span className="font-weight-bold">{price}€</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+        {title && <Card.Title className="text-left">{title}</Card.Title>}
+        {description && (
+          <Card.Text className="text-left mb-5">{description}</Card.Text>
+        )}
+
+        <Media className="align-items-center justify-content-around">
+          <span>Sold by:</span>
+          <Link to={`/auctioneers/${auctioneer_id}`}>
+            <Avatar src={auctioneer_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>Added: {updated_at}</span>
+            {is_owner && auctionPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
+          </div>
+        </Media>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Auction;
---
+++ src/pages/Auctions/AuctionCreateForm.js
@@ -0,0 +1,315 @@
+import { 
+    Button, Row, Col, Image, Form, Card, Alert 
+} from 'react-bootstrap';
+import Upload from "../../assets/upload.png";
+import styles from "../../styles/AuctionCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+import { useRef, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
+
+// Component used for creating a Auctionghetto post.
+// Takes input from the user in the forms and post it to the API
+// Includes error handling that shows an alert to the user.
+function AuctionCreateForm() {
+  useRedirect("loggedOut");
+ 
+  const [errors, setErrors] = useState({});
+ 
+  const [auctionData, setAuctionData] = useState({
+    title: "",
+    productcategory: "home",
+    item_products: 'furniture',
+    autocategory: 'car',
+    auctionstartday: 'monday',
+    description: "",
+    year: "",
+    fueltype: "petrol",
+    price: "",
+    image: "",
+  });
+
+  const {
+    title,
+    productcategory,
+    item_products,
+    autocategory,
+    auctionstartday,
+    description,
+    year,
+    fueltype,
+    price,
+    image,
+  } = auctionData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setAuctionData({
+      ...auctionData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setAuctionData({
+        ...auctionData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+ 
+    formData.append("title", title);
+    formData.append("productcategory", productcategory);
+    formData.append("item_products", item_products);
+    formData.append("autocategory", autocategory);
+    formData.append("auctionstartday", auctionstartday);
+    formData.append("description", description);
+    formData.append("year", year);
+    formData.append("gearbox", gearbox);
+    formData.append("fueltype", fueltype);
+    formData.append("price", price);
+    formData.append("image", imageInput.current.files[0]);
+ 
+    try {
+      const { data } = await axiosReq.post("/auctions/", formData);
+      history.push(`/auctions/${data.id}`);
+    } catch (error) {
+      //console.log(error);
+      if (error.response?.status !== 401) {
+        setErrors(error.response?.data);
+      }
+    }
+  };  
+ 
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Productcategory</Form.Label>
+        <Form.Control
+          as="select"
+          type="text"
+          name="productcategory"
+          value={productcategory}
+          onChange={handleChange}
+        >
+          <option value="home">Home</option>
+          <option value="office">Office</option>
+          <option value="building">Building</option>
+          <option value="vehicle">Vehicle</option>
+          <option value="industry">Industry</option>
+          <option value="agriculture">Agriculture</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Item products</Form.Label>
+        <Form.Control
+          as="select"
+          type="text"
+          name="item_products"
+          value={item_products}
+          onChange={handleChange}
+        >
+          <option value='furniture'>Furniture</option>
+          <option value='chair'>Chair</option>
+          <option value='table'>Table</option>
+          <option value='bicycle'>Bicycle</option>
+          <option value='bed'>Bed</option>
+          <option value='pc'>PC</option>
+          <option value='phones'>Phones</option>
+          <option value='other'>Other</option> 
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Autocategory</Form.Label>
+        <Form.Control
+          as="select"
+          type="text"
+          name="autocategory"
+          value={autocategory}
+          onChange={handleChange}
+        >
+          <option value='car'>Car</option>
+          <option value='minibus'>Minibus</option>
+          <option value='truck'>Truck</option>
+          <option value='bus'>Bus</option>
+          <option value='escavator'>Escavator</option>
+          <option value='motocycle'>Motocycle</option>
+          <option value='other'>Other</option>
+        </Form.Control>
+      </Form.Group>
+
+     <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="description"
+          value={description}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.description?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Year</Form.Label>
+        <Form.Control
+          type="number"
+          min="0"
+          step="1.00"
+          name="year"
+          value={year}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.year?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Autocategory</Form.Label>
+        <Form.Control
+          as="select"
+          type="text"
+          name="autocategory"
+          value={autocategory}
+          onChange={handleChange}
+        >
+          <option value="car">Car</option>
+          <option value="truck">Truck</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Fueltype</Form.Label>
+        <Form.Control
+          as="select"
+          type="text"
+          name="fueltype"
+          value={fueltype}
+          onChange={handleChange}
+        >  
+          <option value='petrol'>Petrol</option>
+          <option value='hybrid'>Hybrid</option>
+          <option value='diesel'>Diesel</option>
+          <option value='electric'>Electric</option>
+          <option value='other'>Other</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Price</Form.Label>
+        <Form.Control
+          type="number"
+          min="0"
+          step="1.00"
+          name="price"
+          value={price}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.price?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => {}}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                src={Upload}
+                message="Click or tap to upload an image"
+              />
+            </Form.Label>
+           )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default AuctionCreateForm;
