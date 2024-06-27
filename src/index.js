57f9a0bf68d781c8706188b3fcb29fa2f0c4891a
Madu-J Omenka <madu.omenka@hotmail.com>
2024-06-27 22:36:20 +0000

    Fixing react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,17 +7,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
 import { AuctioneerDataProvider } from './contexts/AuctioneerDataContext';
 
-ReactDOM.render(
 
-    <Router>
+
+ReactDOM.render(
+  <Router>
       <CurrentUserProvider>
         <AuctioneerDataProvider>
           <App />
         </AuctioneerDataProvider>
       </CurrentUserProvider>
     </Router>,
-  
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
